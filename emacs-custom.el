d071296d56a7219d5c2e692eb011156bfddc3470
Brian Hoffman <bhoffman@loc.gov>
2024-11-04 11:11:04 -0500

    updates

--- emacs-custom.el
+++ emacs-custom.el
@@ -5,10 +5,12 @@
  ;; If there is more than one, they won't work right.
  '(editorconfig-mode t)
  '(magit-define-global-key-bindings 'recommended)
- '(package-selected-packages '(editorconfig yaml-mode yaml-pro magit xref-union)))
+ '(package-selected-packages
+   '(markdown-mode editorconfig yaml-mode yaml-pro magit xref-union)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(keymap-global-set "C-x C-b" 'ibuffer)
